e256f5079eedbcad8ae1604a52011f9fe3abcbb7
Ashish Sharma <ashish.shm09@gmail.com>
2020-08-05 23:09:33 +0530

    Added DOM Display

--- game.js
+++ game.js
@@ -132,3 +132,26 @@ const levelChars = {
 };
 
 let simpleLevel = new Level(simpleLevelPlan);
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+class DOMDisplay {
+  constructor(parent, level) {
+    this.dom = elt("div", { class: "game" }, drawGrid(level));
+    this.actorLayer = null;
+    parent.appendChild(this.dom);
+  }
+
+  clear() {
+    this.dom.remove();
+  }
+}
